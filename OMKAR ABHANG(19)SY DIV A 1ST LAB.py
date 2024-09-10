69e79c16466b7f55fe5ea17c31ceb3e95f57168f
Omkarabhang1234 <omkar.abhang23@pcu.edu.in>
2024-09-10 23:52:26 +0530

    Add files via upload

---
+++ OMKAR ABHANG(19)SY DIV A 1ST LAB.py
@@ -0,0 +1,164 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "f54ee791-80c5-4f07-955b-e7d32031b29e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sum of a and b is 15\n",
+      "subtraction of a and b is 5\n",
+      "multiplication of a and b is 50\n",
+      "division of a and b is 2.0\n",
+      "modulus of a and b is 0\n",
+      "floor division of a and b is 2\n",
+      "exponent of a is 100\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=10\n",
+    "b=5\n",
+    "print(\"sum of a and b is\",a+b)\n",
+    "print(\"subtraction of a and b is\",a-b)\n",
+    "print(\"multiplication of a and b is\",a*b)\n",
+    "print(\"division of a and b is\",a/b)\n",
+    "print(\"modulus of a and b is\",a%b)\n",
+    "print(\"floor division of a and b is\",a//b)\n",
+    "print(\"exponent of a is\",a**2)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 36,
+   "id": "23bdc00c-b695-4820-a28f-13603e7e973a",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "enter the value of a= 2\n",
+      "enter the value or b= 3\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "23\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=int(input(\"enter the value of a=\"))\n",
+    "b=int(input(\"enter the value or b=\"))\n",
+    "#concatenation=a+b\n",
+    "print(\"2\"+\"3\")\n",
+    "\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 34,
+   "id": "537715f1-f033-4840-8f8d-2ffd37d7d7ed",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "False\n",
+      "True\n",
+      "True\n",
+      "True\n",
+      "False\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=200\n",
+    "b=500\n",
+    "print(a==b)\n",
+    "print(a!=b)\n",
+    "print(a<b)\n",
+    "print(a<=b)\n",
+    "print(a>b)\n",
+    "print(a>=b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 45,
+   "id": "7e00c590-e40e-4b79-9d1c-8f83d774051e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "6\n",
+      "4\n",
+      "8\n",
+      "4.0\n",
+      "0.0\n",
+      "0.0\n",
+      "0.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=4\n",
+    "b=2\n",
+    "#add\n",
+    "a+=b\n",
+    "print(a)\n",
+    "#sub\n",
+    "a-=b\n",
+    "print(a)\n",
+    "#mult\n",
+    "a*=b\n",
+    "print(a)\n",
+    "#div\n",
+    "a/=b\n",
+    "print(a)\n",
+    "#mod\n",
+    "a%=b\n",
+    "print(a)\n",
+    "#fl_div\n",
+    "a//=b\n",
+    "print(a)\n",
+    "#exponent\n",
+    "a**=b\n",
+    "print(a)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
